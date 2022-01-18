ed86146183c8c03679144a5ee02d2d3e760d2aa0
bvito73 <97413009+bvito73@users.noreply.github.com>
2022-01-18 23:51:47 +0330

    Add files via upload

---
+++ automobile.py
@@ -0,0 +1,31 @@
+class Automobile:
+    def __init__ (self,make,model,price,mileage):
+        self.__make=make
+        self.__model=model
+        self.__price=price
+        self.__mileage=mileage
+    def set_make(self,make):
+        self.__make=make
+    def set_model(self,model):
+        self.__model=model
+    def set_price(self,price):
+        self.__price
+    def set_mileage(self,mileage):
+        self.__mileage=mileage
+    def get_make(self):
+        return self.__make
+    def get_model(self):
+        return self.__model
+    def get_price(self):
+        return self.__price
+    def get_mileage(self):
+        return self.__mileage
+class Car(Automobile):
+    def __init__(self,make,model,price,mileage,doors):
+        Automobile.__init__(self,make,model,price,mileage)
+        self.__doors=doors
+    def set_doors(self,doors):
+        self.__doors=doors
+    def get_doors(self):
+        return self.__doors
+class  
